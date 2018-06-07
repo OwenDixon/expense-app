1dab1bba0fbcb67de354684c95ef4b150ffdab34
Owen Dixon <owen.dixon23@gmail.com>
2018-06-07 14:57:55 +0100

    Removed dummy expense data

--- src/app.jsx
+++ src/app.jsx
@@ -14,10 +14,6 @@ import 'react-dates/initialize';
 
 const store = configureStore();
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
