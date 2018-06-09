a6088ec4e0aa3e7263bee0d7f676752b764056dd
Owen Dixon <owen.dixon23@gmail.com>
2018-06-09 01:23:14 +0100

    config webpack to creak out css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,48 @@
-const path = require("path");
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: "./src/app.jsx",
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.jsx',
     output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.jsx"
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.jsx'
     },
     module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.jsx$/,
-            exclude: /node_modules/
-        },  {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.jsx$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
     },
-    devtool:"cheap-module-eval-source-map",
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
     }
+  };
 };
