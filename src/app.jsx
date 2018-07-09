c67d08fddfe0e8f7745ada218f35946209d0d5c7
Owen Dixon <owen.dixon23@gmail.com>
2018-07-09 17:10:30 +0100

    updates to fetch data from firebase

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter.jsx';
 import configureStore from './store/configureStore.jsx';
-import { addExpense } from './actions/expenses.jsx';
+import { startSetExpenses } from './actions/expenses.jsx';
 import { setTextFilter } from './actions/filters.jsx';
 import getVisibleExpenses from './selectors/expenses.jsx';
 import 'normalize.css/normalize.css';
@@ -15,12 +15,14 @@ import './firebase/firebase.js';
 
 const store = configureStore();
 
-console.log("test");
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
